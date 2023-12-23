0ae1d3bc50a2b416790ecbfc3f744d0589c08474
Dhanush <dhanushkrish54@gmail.com>
2023-12-23 11:34:27 +0530

    Update and rename Basics to Basics.js

--- Basics.js
+++ Basics.js
@@ -16,3 +16,12 @@ global.cat = 'meow!'
 console.log(global.cat)
 
 Node was designed with server-side web development in mind and has a lot of thoughtful functionality towards that end. At its most simple, however, it provides the ability to run JavaScript programs on our own computers instead of just in the browser’s console or embedded in HTML.
+
+// ************** Running a Program with Node ***************//
+let noun1 = 'cat';
+let adjective = 'silly';
+let noun2 = 'one';
+let verb = 'watch tv';
+let noun3 = 'bread';
+
+console.log(`The world's first ${noun1} was a very ${adjective} ${noun2} who loved to ${verb} while eating ${noun3} for every meal.`);

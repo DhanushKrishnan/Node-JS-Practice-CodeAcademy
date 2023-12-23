a59022ed718ee4e4e4e194c72a12a5eac2a60040
Dhanush <dhanushkrish54@gmail.com>
2023-12-23 12:29:53 +0530

    Update Basics.js | 12:29PM

--- Basics.js
+++ Basics.js
@@ -51,3 +51,28 @@ console.assert(petsArray.length>5);
 └─────────┴──────────┘
 Assertion failed
 
+// *************  Process Module ************** //
+let initialMemory = process.memoryUsage().heapUsed;
+let word = process.argv[2];
+
+console.log(`Your word is ${word}`)
+
+// Create a new array
+let wordArray = [];
+
+// Loop 1000 times, pushing into the array each time 
+for (let i = 0; i < 1000; i++){
+  wordArray.push(`${word} count: ${i}`)
+}
+console.log(`Starting memory usage: ${initialMemory}. \nCurrent memory usage: ${process.memoryUsage().heapUsed}. \nAfter using the loop to add elements to the array, the process is using ${process.memoryUsage().heapUsed - initialMemory} more bytes of memory.`)
+
+
+// ************ OS Module ************** //
+const os = require('os');
+const server = {
+type: os.type(),
+architecture: os.arch(),
+uptime: os.uptime()
+};
+console.log(server);
+
